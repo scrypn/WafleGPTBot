4205a7621f21dab4606bbe5e08cadc6567ea7c18
forgo <127855426+forgooo@users.noreply.github.com>
2023-03-21 16:37:04 +0300

    Update workcsv.py

--- workcsv.py
+++ workcsv.py
@@ -2,28 +2,28 @@ import pandas
 from pathlib import Path
 from csv import writer
 
-def write_data(id, role, request, response):
-    if Path('data/'+role+'_'+str(id)+'.csv').is_file():
-        with open('data/'+role+'_'+str(id)+'.csv', 'a') as file:
+data = {'role': [], 'content': []}
+
+def write_data(id, role, r):
+    if Path('data/data_'+str(id)+'.csv').is_file():
+        with open('data/data_'+str(id)+'.csv', 'a') as file:
             writer_object = writer(file)
-            if role == 'user': data = [request]
-            else: data = [response]
-            writer_object.writerow(data)
+            writer_object.writerow([role, r])
             file.close()
     else:
-        filepath = Path('data/'+role+'_'+str(id)+'.csv')  
+        filepath = Path('data/data_'+str(id)+'.csv')  
         filepath.parent.mkdir(parents=True, exist_ok=True)
-        if role == 'user': df = pandas.DataFrame({'request': []}) 
-        else: df = pandas.DataFrame({'response': []})
+        df = pandas.DataFrame(data)
         df.to_csv(filepath, index=False)
         print('created new csv!')
-        write_data(id, role, request, response)
+        write_data(id, role, r)
         
-def read_data(id, role):
-    file_path = Path('data/'+role+'_'+str(id)+'.csv')
+def read_data(id):
+    file_path = Path('data/data_'+str(id)+'.csv')
     if file_path.is_file():
         df = pandas.read_csv(file_path)
-        return list(df['request'])  
+        print(df)
+        return list(df['content'])
     else:
         print('No such file exists!')
         return None
