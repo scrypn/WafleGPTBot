e21c27218e4db177455d92a10881eed2db0a5a79
forgo <127855426+forgooo@users.noreply.github.com>
2023-03-21 18:38:40 +0300

    Update waflegpt.py

--- waflegpt.py
+++ waflegpt.py
@@ -2,7 +2,9 @@ import telebot
 import openai
 import time
 import pandas as pd
-from config import apiKeyTelegram, apiKeyOpenAI
+from config import *
+import workcsv
+
 
 bot = telebot.TeleBot(apiKeyTelegram)
 openai.api_key = apiKeyOpenAI
@@ -10,28 +12,23 @@ openai.api_key = apiKeyOpenAI
 queue = []
 
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: 'wafle' in message.text.lower() or 'вафл' in message.text.lower())
 def generate_reply(message):
     queue.append(message)
-    botMessage = bot.reply_to(message, "...")
+    botMessage = bot.reply_to(message, '...')
     botMessage2 = ''
     isBotMessage2Visable = False
     lastVal = ''
     maxChars = 3000
     while True:
         if queue[0] == message:
+            workcsv.write_data(message.from_user.id, 'user', message.text)
             response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                max_tokens=2048,
-                temperature=0.5,
+                model=model,
+                max_tokens=max_tokens,
+                temperature=temperature,
                 stream=True,
-                messages=[
-                    {"role": "system",
-                     "content": "Тебя зовут WafleGPT или Вафля и ты создана двумя мирэашниками Колей и Ваней"},
-                    {"role": "user", "content": "Привет, как твои делишки"},
-                    {"role": "assistant", "content": "Привет, у меня всё здорово"},
-                    {"role": "user", "content": message.text},
-                ]
+                messages=workcsv.read_data(message.from_user.id)[-7:]
             )
 
             collected_messages = []
@@ -39,8 +36,8 @@ def generate_reply(message):
             for chunk in response:
                 counter += 1
                 chunk_message = chunk['choices'][0]['delta']
-                collected_messages.append(chunk_message)
 
+                collected_messages.append(chunk_message)
                 if counter >= 150:
                     full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
                     if full_reply_content != '':
@@ -53,7 +50,7 @@ def generate_reply(message):
                             else:
                                 bot.edit_message_text(
                                     full_reply_content[
-                                    :maxChars] + "\n\nДальнейшие строки перенесены в другое сообщение",
+                                    :maxChars] + '\n\nДальнейшие строки перенесены в другое сообщение',
                                     botMessage.chat.id, botMessage.message_id)
                                 botMessage2 = bot.reply_to(botMessage, full_reply_content[maxChars:])
                                 isBotMessage2Visable = True
@@ -61,6 +58,7 @@ def generate_reply(message):
                         counter = 0
 
             full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
+            workcsv.write_data(message.from_user.id, 'assistant', full_reply_content)
             if full_reply_content != '':
                 if len(full_reply_content) < maxChars:
                     bot.edit_message_text(full_reply_content, botMessage.chat.id, botMessage.message_id)
@@ -70,7 +68,7 @@ def generate_reply(message):
                             full_reply_content[maxChars:], botMessage2.chat.id, botMessage2.message_id)
                     else:
                         bot.edit_message_text(
-                            full_reply_content[:maxChars] + "\n\nДальнейшие строки перенесены в другое сообщение",
+                            full_reply_content[:maxChars] + '\n\nДальнейшие строки перенесены в другое сообщение',
                             botMessage.chat.id, botMessage.message_id)
                         botMessage2 = bot.reply_to(botMessage, full_reply_content[maxChars:])
                         isBotMessage2Visable = True
@@ -79,11 +77,10 @@ def generate_reply(message):
             break
         else:
             if str(queue.index(message)) != lastVal:
-                bot.edit_message_text("Вы " + str(queue.index(message)) + " в очереди", botMessage.chat.id,
+                bot.edit_message_text('Вы ' + str(queue.index(message)) + ' в очереди', botMessage.chat.id,
                                       botMessage.message_id)
                 lastVal = str(queue.index(message))
 
         time.sleep(5)
-
 
 bot.infinity_polling(none_stop=True, interval=0, timeout=30)
