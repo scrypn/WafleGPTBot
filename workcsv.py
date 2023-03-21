e379f53dd4f41d73dd5c1d8ff127201427463820
forgo <127855426+forgooo@users.noreply.github.com>
2023-03-21 15:08:59 +0300

    Update workcsv.py

--- workcsv.py
+++ workcsv.py
@@ -6,16 +6,24 @@ def write_data(id, role, request, response):
     if Path('data/'+role+'_'+str(id)+'.csv').is_file():
         with open('data/'+role+'_'+str(id)+'.csv', 'a') as file:
             writer_object = writer(file)
-            writer_object.writerow([request if role == 'user' else response])
+            if role == 'user': data = [request]
+            else: data = [response]
+            writer_object.writerow(data)
             file.close()
     else:
         filepath = Path('data/'+role+'_'+str(id)+'.csv')  
         filepath.parent.mkdir(parents=True, exist_ok=True)
-        par = 'request' if role == 'user' else 'response'
-        df = pandas.DataFrame({par: []})
-        df.to_csv(filepath)
+        if role == 'user': df = pandas.DataFrame({'request': []}) 
+        else: df = pandas.DataFrame({'response': []})
+        df.to_csv(filepath, index=False)
         print('created new csv!')
         write_data(id, role, request, response)
         
-def read_data(id):
-    pass
+def read_data(id, role):
+    file_path = Path('data/+role+_'+str(id)+'.csv')
+    if file_path.is_file():
+        df = pandas.read_csv(file_path)
+        return list(df['request'])  
+    else:
+        print('No such file exists!')
+        return None
